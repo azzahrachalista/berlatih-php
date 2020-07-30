93fefa9af7e7008a0d3a7ed022a85efcf031b689
azzahrachalista <azzahrachalistap@gmail.com>
2020-07-30 10:18:27 +0700

    message

---
+++ palindrome-angka.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Looping</title>
+</head>
+<body>
+    <h1>Berlatih Looping</h1>
+
+    <?php 
+       function palindrome_angka($angka) {
+        // tulis kode di sini
+      }
+      
+      // TEST CASES
+      echo palindrome_angka(8) // 9
+      echo palindrome_angka(10); // 11
+      echo palindrome_angka(117); // 121
+      echo palindrome_angka(175); // 181
+      echo palindrome_angka(1000); // 1001
+
+      
+    ?>
+
+</body>
+</html>
